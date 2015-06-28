2e1df0000222bd1123cd559c51833a13ba37e6af
Ting Lew <ting.lew@gmail.com>
2015-06-28 01:27:38 -0400

    changed modal box sizing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'posts#index'
-  devise_for :users
+  devise_for :users, :controllers => {sessions: 'sessions', registrations: 'registrations'}
   resources :users
   resources :posts
 end
