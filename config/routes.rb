736900c10ee08477b30822737b6f3e56ad077ebb
Ting Lew <ting.lew@gmail.com>
2015-06-29 12:50:55 -0400

    undo devise SPA config

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'posts#index'
-  devise_for :users, :controllers => {sessions: 'sessions', registrations: 'registrations'}
+  devise_for :users
   resources :users
   resources :posts
 end
