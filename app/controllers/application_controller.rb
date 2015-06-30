405c6bf1e3678153ca425ae61be2e1f1cbdb7c88
Ting Lew <ting.lew@gmail.com>
2015-06-30 17:31:00 -0400

    users must sign in before posting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,7 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(
       :name,:email, :password, :description, :avatar) }
+
     devise_parameter_sanitizer.for(:account_update) { |u| u.permit(
       :name,:email, :password, :password_confirmation, :current_password, :description, :avatar) }
   end
