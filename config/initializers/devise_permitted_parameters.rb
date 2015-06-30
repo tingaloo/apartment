105b816cae79e670def9a61c3510d37a55f0dc41
Ting Lew <ting.lew@gmail.com>
2015-06-29 21:56:54 -0400

    devise paramters on module

--- config/initializers/devise_permitted_parameters.rb
+++ config/initializers/devise_permitted_parameters.rb
@@ -8,8 +8,11 @@ module DevisePermittedParameters
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :name
-    devise_parameter_sanitizer.for(:account_update) << :name
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(
+      :name,:email, :password, :description, :avatar) }
+
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(
+      :name,:email, :password, :password_confirmation, :current_password, :description, :avatar) }
   end
 
 end
