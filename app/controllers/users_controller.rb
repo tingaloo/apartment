29117dd0b11457b0468db6aebe097f3e6b4a14a6
Ting Lew <ting.lew@gmail.com>
2015-06-29 21:23:56 -0400

    cleaned up devise

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:avatar, :description)
+    params.require(:user).permit(:avatar)
   end
 
 end
